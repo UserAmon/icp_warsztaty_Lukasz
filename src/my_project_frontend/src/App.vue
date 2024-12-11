ff5ed5ae5b8d695eae949b99d34059206128f1b7
UserAmon L <leszczluki@gmail.com>
2024-12-11 10:23:09 +0000

    Initialization

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -21,7 +21,7 @@ async function handleSubmit(e) {
     <form action="#" @submit="handleSubmit">
       <label for="name">Enter your name: &nbsp;</label>
       <input id="name" alt="Name" type="text" />
-      <button type="submit">Click Me!</button>
+      <button type="submit">Click Me HARDER DADDY!</button>
     </form>
     <section id="greeting">{{ greeting }}</section>
   </main>
